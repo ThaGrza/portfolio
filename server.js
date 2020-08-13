038c20c057db0b8292802f0a25d60f1b453e4c58
ThaGrza <andrewgriswold7@gmail.com>
2020-08-13 18:32:13 -0500

    broken

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const PORT   = process.env.PORT || 3001;
 const app = express();
 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static("public"));
+
 if(process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 };
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(_dirname, "./client/build/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, function(){

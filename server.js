b8451ad99c4c384cee610217f1548c963a1e1273
ThaGrza <andrewgriswold7@gmail.com>
2020-08-13 19:54:14 -0500

    server connected

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("public"));
 
 if(process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));

6a1e6438195e88fd800c80c2fc2c3d0e05092da3
ThaGrza <andrewgriswold7@gmail.com>
2020-09-28 17:35:14 -0500

    new homepage layout

--- src/components/pages/Homepage/index.js
+++ src/components/pages/Homepage/index.js
@@ -7,9 +7,10 @@ function Homepage (props) {
   return (
   <div className="homepage-container">
     <div className="homepage">
+      <h3 className="skills-header">Welcome!</h3>
+      <p>The purpose of this site is to display projects i've worked on and give a little insight to who I am!</p>
       <img className="profile-pic" alt="🧙‍♂️" src={Profilepic}></img>
-      <p>Welcome to my personal portfolio! The purpose of this site is to display projects i've worked on and give a little insight to who I am!</p>
-      <h3 className="header">Who I Am</h3>
+      <h3 className="header">Who Am I?</h3>
       <p>My name is Andrew Griswold and I am a 25 year old programmer. I currently live in Austin, Texas where I studied Computer Science and programming to become a full
 stack developer. My passion for computers and science grew from curiosity I got from modding video games like Grand Theft Auto and playing classic nintendo-64 games like Mario as a kid. 
 I love learning new technologies and challenging myself to create new things. You can see some open source projects I have created and worked on in the Portfolio tab, if you see anything

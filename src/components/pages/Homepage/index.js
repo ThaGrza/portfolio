1c53f9194a36531456f1fcc0bbd524cf349b5e63
ThaGrza <andrewgriswold7@gmail.com>
2020-09-28 14:43:27 -0500

    added subsections and skills

--- src/components/pages/Homepage/index.js
+++ src/components/pages/Homepage/index.js
@@ -7,12 +7,21 @@ function Homepage (props) {
   return (
   <div className="homepage-container">
     <div className="homepage">
-    <img className="profile-pic" alt="🧙‍♂️" src={Profilepic}></img>
+      <img className="profile-pic" alt="🧙‍♂️" src={Profilepic}></img>
       <p>Welcome to my personal portfolio! The purpose of this site is to display projects i've worked on and give a little insight to who I am!</p>
+      <h3 className="header">Who I Am</h3>
       <p>My name is Andrew Griswold and I am a 25 year old programmer. I currently live in Austin, Texas where I studied Computer Science and programming to become a full
 stack developer. My passion for computers and science grew from curiosity I got from modding video games like Grand Theft Auto and playing classic nintendo-64 games like Mario as a kid. 
 I love learning new technologies and challenging myself to create new things. You can see some open source projects I have created and worked on in the Portfolio tab, if you see anything
 that interests you or you want to get in contact for any reason, feel free to send an email via the Contact page!</p>
+      <h3 className="skills-header">What I Know</h3>
+      <ul className="skills-list">
+        <li>Javascript</li>
+        <li>React</li>
+        <li>MongoDB</li>
+        <li>MySql</li>
+      </ul>
+
     </div>
     <Footer />
   </div>

7d4b3bf2b6009fe4aa9e77b82398e92a28aa7ef8
ThaGrza <andrewgriswold7@gmail.com>
2020-09-29 21:09:11 -0500

    website info error/ card css

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const Card = props => (
   <div className="card-component">
-    <a target="_blank" rel="noopener noreferrer" href={props.url}>
+    <a  className="img-container" target="_blank" rel="noopener noreferrer" href={props.url}>
       <img className="card-img" alt=":(" src={props.image} />
     </a>
+    <div className="card-info-container">
     <p><strong>Tech used: </strong>{props.tech} </p>
     <p><strong>Github:</strong><a target="_blank" rel="noopener noreferrer" href={props.githubUrl}> {props.name}</a> </p>
+    </div>
   </div>
 )
 

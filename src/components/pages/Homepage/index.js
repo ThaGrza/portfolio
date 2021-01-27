71ff4210e2bc8227d7612218c8d327907012e6d3
ThaGrza <andrewgriswold7@gmail.com>
2021-01-26 22:48:56 -0500

    adding netflix-clone

--- src/components/pages/Homepage/index.js
+++ src/components/pages/Homepage/index.js
@@ -3,7 +3,7 @@ import Profilepic from "./profile2.png";
 import "./index.css";
 import Footer from "../../Footer/index";
 
-function Homepage (props) {
+function Homepage () {
   return (
   <div className="homepage-container">
     <div className="homepage">
@@ -12,7 +12,7 @@ function Homepage (props) {
       <img className="profile-pic" alt="🧙‍♂️" src={Profilepic}></img>
       <h3 className="header">Who Am I?</h3>
       <p>My name is Andrew Griswold and I am a 25 year old programmer. I currently live in Austin, Texas where I studied Computer Science and programming to become a full
-stack developer. My passion for computers and science grew from curiosity I got from modding video games like Grand Theft Auto and playing classic nintendo-64 games like Mario as a kid. 
+stack developer. My passion for computers and science grew from curiosity I got from modding video games like Grand Theft Auto and playing classic nintendo-64 games like Mario as a kid.
 I love learning new technologies and challenging myself to create new things. You can see some open source projects I have created and worked on in the Portfolio tab, if you see anything
 that interests you or you want to get in contact for any reason, feel free to send an email via the Contact page!</p>
       <h3 className="skills-header">What I Know</h3>

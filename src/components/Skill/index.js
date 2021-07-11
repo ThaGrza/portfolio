a986580fea4cac2b07ac9602fe0b96cdc14bc4d4
ThaGrza <andrewgriswold7@gmail.com>
2021-07-10 20:27:01 -0400

    skills added/homepage about me

--- src/components/Skill/index.js
+++ src/components/Skill/index.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Skill = props => (
     <div className="skill">
+        <img src={props.icon} alt="icon" className="skillIcon"/>
         <h4 className="skillHeader"> {props.title} </h4>
         <div className="skillPercent" style={{ gridColumnEnd: props.value }}></div>
+        <p className="percentStart">Very Little</p>
+        <p className="percentEnd">Everything</p>
     </div>
 )
 
--- src/components/pages/Homepage/index.js
+++ src/components/pages/Homepage/index.js
@@ -10,23 +10,28 @@ function Homepage () {
   <div className="homepage-container">
     <div className="headerContainer">
       <h1 className="nameHeader">I'm Andrew Griswold.</h1>
-      <h3 className="headerSubtext">A fullstack software engineer based in Boston, Massachusetts</h3>
     </div>
     <div className="aboutMe">
       <h1 className="aboutHeader">About Me</h1>
       <img src={profilePic} alt="Profile" className="headshot" />
-      <p className="aboutText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit,Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      <p className="aboutText">I am a fullstack web developer based in Boston, Massachusetts.
+I love coding and turning great ideas into a reality. I am passionate about learning new technologies
+to help myself grow as a developer.
+I learned the basics of programming from Austin Community College and went on to learn full stack web development
+through a bootcamp at The University of Texas at Austin. I continued learning fullstack development on my own while building 
+various websites and apps.
+
+I am currently freelancing and contributing to open source as I look for more perminent opportunities.
       </p>
     </div>
     <div className="edu">
       <h1 className="eduHeader">Education</h1>
-      <ul className="eduInfo">
-        <li className="listElement">Exeter High School</li>
-        <li className="listElement">Austin Community College</li>
-        <li className="listElement">UT Bootcamp</li>
-      </ul>
+      <div className="eduInfo">
+        <h2 className="eduItem">University of Texas at Austin</h2>
+        <h4 className="eduSubtext">Fullstack Web Developer certificate • 2019-2020</h4>
+        <h2 className="eduItem">Austin Community College</h2>
+        <h4 className="eduSubtext">Associates of Computer Science • 2018-2020</h4>
+      </div>
     </div>
     <div className="skills">
       <h1 className="skillsHeader">Skills & Experience</h1>
@@ -34,7 +39,7 @@ function Homepage () {
         {SkillsList.map(SkillsList => (
           <Skill
           title={SkillsList.title}
-          value={SkillsList.vale}
+          value={SkillsList.value}
           />
         ))}
       </div>

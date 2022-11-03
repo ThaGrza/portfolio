5a69d84d4904e7315883615a5f022e934cc7b0ce
Andrew <andrewgriswold7@gmail.com>
2022-11-03 16:24:42 -0400

    portfolio page cards redone

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,22 +4,22 @@ import GithubIcon from "../../assets/icons/githubIcon.png";
 
 const Card = props => (
   <div className="card-component">
-    <a className="img-container" target="_blank" rel="noopener noreferrer" href={props.url}>
+    <a className="card-img-container" target="_blank" rel="noopener noreferrer" href={props.url}>
       <img className="card-img" alt="View Website"href={props.url} src={props.image} />
     </a>
     <div className="card-info-container">
       <h2 className="card-info-title">{props.name}</h2>
       <p className="card-description">{props.description}</p>
     </div>
+    <a className="github-icon-container" target="_blank" rel="noopener noreferrer" href={props.githubUrl}>
+        <img className="github-icon" alt="Github" src={GithubIcon} />
+      </a>
       <div className="icon-container">
         <p className="icon-title">Built with:</p>
         <img className="icon-image" alt=":(" src={props.tech}  />
         <img className="icon-image" alt=":(" src={props.tech2} />
         <img className="icon-image" alt=":(" src={props.tech3} />
       </div>
-      <a className="img-container" target="_blank" rel="noopener noreferrer" href={props.githubUrl}>
-        <img className="github-icon" alt="Github" src={GithubIcon} />
-      </a>
   </div>
 )
 
